9d59d2f693efbc0b3ca4db6b2a2b8d4cab41b336
gileslin1213 <33174637+gileslin1213@users.noreply.github.com>
2018-10-19 00:34:03 +0800

    132

--- js/main.js
+++ js/main.js
@@ -1,127 +1,126 @@
 
 $(document).ready(function () {
 
-  $('.anchor').on('click', function (event) {
-      event.preventDefault();
-      $('html, body').animate({
-          scrollTop: Math.ceil($($.attr(this, 'href')).offset().top - 88)
-      }, 600);
-      $('.collapse').collapse('hide')
-  });
+    $('.anchor').on('click', function (event) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: Math.ceil($($.attr(this, 'href')).offset().top - 88)
+        }, 600);
+        $('.collapse').collapse('hide')
+    });
 
 });
 
 
 $(document).ready(function () {
     //initialize swiper when document ready
-    var mySwiper = new Swiper ('#home_swiper', {
-      // Optional parameters
-      direction: 'horizontal',
-      loop: true,
-      autoplay: {
-        delay: 5000,
-      },
-      
-      // If we need pagination
-    pagination: {
-        el: '#home_swiper .swiper-pagination',
-      },
-  
-      // Navigation arrows
-      navigation: {
-        nextEl: '#home_swiper .swiper-button-next',
-        prevEl: '#home_swiper .swiper-button-prev',
-      },
-  
-      // And if we need scrollbar
-      scrollbar: {
-        el: '#home_swiper .swiper-scrollbar',
-      }
+    var mySwiper = new Swiper('#home_swiper', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+        autoplay: {
+            delay: 5000,
+        },
+
+        // If we need pagination
+        pagination: {
+            el: '#home_swiper .swiper-pagination',
+        },
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '#home_swiper .swiper-button-next',
+            prevEl: '#home_swiper .swiper-button-prev',
+        },
+
+        // And if we need scrollbar
+        scrollbar: {
+            el: '#home_swiper .swiper-scrollbar',
+        }
     })
-  });
+});
 
 
 
-  $(document).ready(function () {
+$(document).ready(function () {
     //initialize swiper when document ready
-    var mySwiper = new Swiper ('#movie_time_swiper2', {
-      // Optional parameters
-      speed:1000,
-      slidesPerView: 4,
-      spaceBetween: 40,
-      loop:true,
-      direction: 'horizontal',
-      freeMode:true,
-      
-      // If we need pagination
-      navigation: {
-        nextEl: '#movie_time_swiper2 .swiper-button-next-item',
-        prevEl: '#movie_time_swiper2 .swiper-button-prev-item',
-      },
-      breakpoints: {
-        768: {
-          slidesPerView: 1,
-          spaceBetween: 30,
+    var mySwiper = new Swiper('#movie_time_swiper2', {
+        // Optional parameters
+        speed: 1000,
+        slidesPerView: 4,
+        spaceBetween: 40,
+        loop: true,
+        direction: 'horizontal',
+        freeMode: true,
+
+        // If we need pagination
+        navigation: {
+            nextEl: '#movie_time_swiper2 .swiper-button-next-item',
+            prevEl: '#movie_time_swiper2 .swiper-button-prev-item',
+        },
+        breakpoints: {
+            768: {
+                slidesPerView: 1,
+                spaceBetween: 30,
+            }
         }
-      }
-     
+
     })
-    var mySwiper = new Swiper ('#movie_time_swiper1', {
-      // Optional parameters
-      speed:1000,
-      slidesPerView: 4,
-      spaceBetween: 40,
-      loop:true,
-      direction: 'horizontal',
-      freeMode:true,
-      
-      // If we need pagination
-      navigation: {
-        nextEl: '#movie_time_swiper1 .swiper-button-next-item',
-        prevEl: '#movie_time_swiper1 .swiper-button-prev-item',
-      },
-      breakpoints: {
-        768: {
-          slidesPerView: 1,
-          spaceBetween: 30,
+    var mySwiper = new Swiper('#movie_time_swiper1', {
+        // Optional parameters
+        speed: 1000,
+        slidesPerView: 4,
+        spaceBetween: 40,
+        loop: true,
+        direction: 'horizontal',
+        freeMode: true,
+
+        // If we need pagination
+        navigation: {
+            nextEl: '#movie_time_swiper1 .swiper-button-next-item',
+            prevEl: '#movie_time_swiper1 .swiper-button-prev-item',
+        },
+        breakpoints: {
+            768: {
+                slidesPerView: 1,
+                spaceBetween: 30,
+            }
         }
-      }
-     
+
     })
-  });
+});
 
+$(document).ready(function () {
+    $(('#movie_time_swiper2')).addClass('d-none');
+    $('.filter').click(function (e) {
+        e.preventDefault();
+        var val = $(this).attr('data-filter');
+        $('.filter.bg-danger').removeClass('bg-danger')
+        $(this).addClass('bg-danger');
+        $(val).addClass('d-none');
+        if (val == '#movie_time_swiper2') {
+            $('#movie_time_swiper1').removeClass('d-none');
+        } else {
+            $('#movie_time_swiper2').removeClass('d-none');
+        }
 
-  $(document).ready(function () {
-
-    $("#movie_time_swiper2").addClass('d-none');
-    $("[data-filter='1']").addClass('bg-danger');
-    $('.filter').click(function (e) { 
-      e.preventDefault();
-      $('.filter.bg-danger').removeClass('bg-danger')
-      $(this).addClass('bg-danger');
-      if($(this).attr('data-filter') == 1){
-        $('#movie_time_swiper2').addClass('d-none');
-        $('#movie_time_swiper1').removeClass('d-none');
-      }else{
-        $('#movie_time_swiper1').addClass('d-none');
-        $('#movie_time_swiper2').removeClass('d-none');
-      }
-      
     });
-  });
+});
 
 
 
-  $(document).ready(function () {
+
+
+$(document).ready(function () {
     $('#myModal').modal('hide');
-    });
+});
 
-    // $('body').scrollspy({ target: 'nav' })
-function get_information(){
-  $.getJSON("/json/information.json",
-    function (data) {
-        console.log(data)
-    }
-  );
+// $('body').scrollspy({ target: 'nav' })
+function get_information() {
+    $.getJSON("/json/information.json",
+        function (data) {
+            console.log(data)
+        }
+    );
 }
 
